d184579716ce3a1e3fe3acafa505a2264abb7e36
David Martínez <goddrinksjava@gmail.com>
2023-01-30 14:19:35 -0600

    first commit

---
+++ app/src/main/java/traductores/BasicSym.java
@@ -0,0 +1,17 @@
+package traductores;
+
+public class BasicSym {
+  public final static int EOF = 10;
+  public final static int SUMA = 11;
+  public final static int RESTA = 12;
+  public final static int MULTIPLICACION = 13;
+  public final static int DIVISION = 14;
+  public final static int RESIDUO = 15;
+  public final static int MAYOR = 16;
+  public final static int MENOR = 17;
+  public final static int MAYOR_IGUAL = 18;
+  public final static int MENOR_IGUAL = 19;
+  public final static int IGUAL = 20;
+  public final static int DIFERENTE = 21;
+  public final static int ASIGNACION = 22;
+}
